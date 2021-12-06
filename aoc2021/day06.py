fa11362bb67756b91151075997dd68fe5481b757
Mike Vink <code@vink.tech>
2021-12-06 23:29:51 +0000

    day 06

---
+++ aoc2021/day06.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from aoc2021 import input
+from aoc2021 import error
+
+DAY: str = "06"
+
+def load_fish(input_file: str) -> list[int]:
+    lines: list[str] = input.load_lines(DAY, input_file)
+    return [ int(x) for x in lines[0].split(",")]
+
+class School:
+    def __init__(self, initial: list[int], lifecycle: int, initial_offset: int):
+        self.lifecycle: int = lifecycle - 1
+        self.initial_lifecycle: int = self.lifecycle + initial_offset
+        self.stages: int = self.initial_lifecycle + 1
+        self.timers: list[int] = [0] * self.stages
+        for x in initial:
+            self.timers[x] += 1
+
+    def tic(self):
+        new_timers: list[int] = [0] * self.stages
+        for i in range(self.stages):
+            if 0 == i:
+                new_timers[self.initial_lifecycle] += self.timers[i]
+                new_timers[self.lifecycle] += self.timers[i]
+            else:
+                new_timers[i-1] += self.timers[i]
+        self.timers = new_timers
+
+    def simulate(self, days: int):
+        for i in range(days):
+            self.tic()
+
+    def population(self) -> int:
+        return sum(self.timers)
+
+def simulation(input_file: str, days: int) -> int:
+    school: School = School(load_fish(input_file), 7, 2)
+    school.simulate(days)
+    return school.population()
+
+
+def part01(input_file: str) -> str:
+    return str(simulation(input_file, 80))
+
+def part02(input_file: str) -> str:
+    return str(simulation(input_file, 256))
