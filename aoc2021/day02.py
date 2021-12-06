7fb187724f6506537f548b49a17606b47b399e67
Mike Vink <code@vink.tech>
2021-12-06 16:47:19 +0000

    day 02

---
+++ aoc2021/day02.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from aoc2021 import input
+
+DAY: str = "02"
+
+def part01(input_file: str) -> str:
+    lines: list[str] = input.load_lines(DAY, input_file)
+    position: int = 0
+    depth: int = 0
+    for line in lines:
+        modifier, value = line.split(" ")
+        value = int(value)
+        if "forward" == modifier:
+            position += value
+        elif "up" == modifier:
+            depth -= value
+        elif "down" == modifier:
+            depth += value
+        else:
+            return "++?????++ Out of Cheese Error. Redo From Start."
+    return str(position * depth)
+
+def part02(input_file: str) -> str:
+    lines: list[str] = input.load_lines(DAY, input_file)
+    position: int = 0
+    depth: int = 0
+    aim: int = 0
+    for line in lines:
+        modifier, value = line.split(" ")
+        value = int(value)
+        if "forward" == modifier:
+            position += value
+            depth += aim * value
+        elif "up" == modifier:
+            aim -= value
+        elif "down" == modifier:
+            aim += value
+        else:
+            return "++?????++ Out of Cheese Error. Redo From Start."
+    return str(position * depth)
