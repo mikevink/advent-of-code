c9f39076b98e6246208cdce1527ee8e5ed4f2939
Mike Vink <code@vink.tech>
2021-12-11 20:06:39 +0000

    partial day 11, need a proper debugger. Termux/Vim aren't great if you're knackered

---
+++ aoc/year2021/day11/__init__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from aoc.common import input
+from aoc.common import error
+
+DAY: str = "2021/11"
+
+ROWS: int = 10
+COLS: int = 10
+DIRECTIONS: list[tuple[int, int]] = [ (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (-1, 0), (-1, -1) ]
+
+def to_array(line: str) -> list[int]:
+    return [int(l) for l in line]
+
+def flash(array: list[list[int]], i: int, j: int):
+    if 0 == array[i][j]:
+        return
+    array[i][j] = 0
+    for d in DIRECTIONS:
+        ni: int = i + d[0]
+        nj: int = j + d[1]
+        if 0 <= ni < ROWS and 0 <= nj < COLS:
+            if 0 == array[ni][nj]:
+                continue
+            array[ni][nj] += 1
+            if 9 < array[ni][nj]:
+                flash(array, ni, nj)
+
+def print_array(array: list[list[int]], step: int):
+    print(f"At step {step}")
+    for row in array:
+        print("".join(map(str, row)))
+    print("")
+
+
+def part01(input_file: str, steps: int) -> str:
+    if "input" == input_file:
+        return error.ERROR
+    array: list[list[int]] = input.load_lines(DAY, input_file, to_array)
+    flashes: int = 0
+    print_array(array, 0)
+    for step in range(steps):
+        for i in range(ROWS):
+            for j in range(COLS):
+                array[i][j] += 1
+        for i in range(ROWS):
+            for j in range(COLS):
+                if 9 < array[i][j]:
+                    flash(array, i, j)
+        for row in array:
+            for col in row:
+                 if 0 == col:
+                     flashes += 1
+        print_array(array, step + 1)
+    return str(flashes)
+
+
+def part02(input_file: str) -> str:
+    lines: list[str] = input.load_lines(DAY, input_file)
+    return error.ERROR
---
+++ test/2021/test_day11.py
@@ -0,0 +1,20 @@
+from aoc.year2021 import day11
+
+
+def test_year2021_day11_part01_sample():
+    assert "204" == day11.part01("sample", 10)
+    #assert "1656" == day11.part01("sample", 100)
+
+
+def test_year2021_day11_part01_input():
+    result: str = day11.part01("input", 100)
+    print(f"Day 11 Part 01 Result: {result}")
+
+
+def test_year2021_day11_part02_sample():
+    assert "-" == day11.part02("sample")
+
+
+def test_year2021_day11_part02_input():
+    result: str = day11.part02("input")
+    print(f"Day 11 Part 02 Result: {result}")
