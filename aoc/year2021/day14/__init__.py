3220e2297957c9e471f40d8879abbb39dba1f143
Mike Vink <code@vink.tech>
2021-12-14 22:30:22 +0000

    break the chain (we only really care about the pairs of chain nodes, not teh actual chain itself. and those ain't many)

--- aoc/year2021/day14/__init__.py
+++ aoc/year2021/day14/__init__.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
-from typing import Optional
+from collections import namedtuple
 
 from aoc.common import input
-from aoc.common import error
 
 DAY: str = "2021/14"
 
+Rule: namedtuple = namedtuple("Rule", ["node", "left_link", "right_link"])
 
-def parse_rules(lines: list[str]) -> dict[str, str]:
-    rules: dict[str, str] = {}
+
+def parse_rules(lines: list[str]) -> dict[str, Rule]:
+    rules: dict[str, tuple[str, str, str]] = {}
     for line in lines:
         condition, result = line.split(" -> ")
-        rules[condition] = result
+        rules[condition] = Rule(result, f"{condition[0]}{result}", f"{result}{condition[1]}")
     return rules
 
 
@@ -23,33 +24,55 @@ def count_initial_chain(chain: str, counts: dict[str, int]):
             counts[node] = 1
 
 
-def link(chain: str, rules: dict[str, str], counts: dict[str, int]) -> str:
-    linked_chain: str = ""
+def break_chain(chain: str) -> dict[str, int]:
+    links: dict[str, int] = {}
     for i in range(len(chain) - 1):
-        pair: str = chain[i:i + 2]
-        if pair in rules:
-            node: str = rules[pair]
-            linked_chain = f"{linked_chain}{chain[i]}{node}"
-            if node in counts:
-                counts[node] += 1
-            else:
-                counts[node] = 1
+        chainlink: str = chain[i : i + 2]
+        if chainlink not in links:
+            links[chainlink] = 1
         else:
-            linked_chain = f"{linked_chain}{chain[i]}"
-    return f"{linked_chain}{chain[-1]}"
+            links[chainlink] += 1
+    return links
+
+
+def index_possible_links(links: dict[str, int], rules: dict[str, Rule]):
+    for link, rule in rules.items():
+        if link not in links:
+            links[link] = 0
+        if rule.left_link not in links:
+            links[rule.left_link] = 0
+        if rule.right_link not in links:
+            links[rule.right_link] = 0
+
+
+def index_possible_nodes(counts: dict[str, int], rules: dict[str, Rule]):
+    for rule in rules.values():
+        if rule.node not in counts:
+            counts[rule.node] = 0
+
+
+def grow_chain(links: dict[str, int], rules: dict[str, Rule], counts: dict[str, int]):
+    for link, num_links in list(links.items()):
+        if 0 != num_links and link in rules:
+            links[link] -= num_links
+            links[rules[link].left_link] += num_links
+            links[rules[link].right_link] += num_links
+            counts[rules[link].node] += num_links
 
 
 def polymerise(input_file: str, steps: int) -> int:
     lines: list[str] = input.load_lines(DAY, input_file)
     chain: str = lines.pop(0)
-    # get rid of the blank line
-    lines.pop(0)
-    rules: dict[str, str] = parse_rules(lines)
     counts: dict[str, int] = {}
     count_initial_chain(chain, counts)
+    links: dict[str, int] = break_chain(chain)
+    # get rid of the blank line
+    lines.pop(0)
+    rules: dict[str, tuple[str, str, str]] = parse_rules(lines)
+    index_possible_links(links, rules)
+    index_possible_nodes(counts, rules)
     for _ in range(steps):
-        chain = link(chain, rules, counts)
-        print(_)
+        grow_chain(links, rules, counts)
 
     maxn: int = max(counts.values())
     minn: int = min(counts.values())
