a93cff88dac585081ee05b8de4671c28281a196d
Mike Vink <code@vink.tech>
2021-12-14 21:48:30 +0000

    even simpler, almost there I think

--- aoc/year2021/day14/__init__.py
+++ aoc/year2021/day14/__init__.py
@@ -7,52 +7,6 @@ from aoc.common import error
 DAY: str = "2021/14"
 
 
-class Node:
-    def __init__(self, value: str):
-        self.value: str = value
-        self.next: "Node" = None
-        self.prev: "Node" = None
-        self.link_node: "Node" = None
-
-    @property
-    def pair(self) -> Optional[str]:
-        if self.has_next():
-            return f"{self.value}{self.next.value}"
-        return None
-
-    def has_next(self) -> bool:
-        return self.next is not None
-
-    def link(self, node: "Node"):
-        if self.has_next():
-            self.link_node = node
-
-    def chain(self):
-        if self.has_next() and self.link_node is not None:
-            self.next.prev = self.link_node
-            self.link_node.next = self.next
-            self.next = self.link_node
-            self.link_node.prev = self
-            self.link_node = None
-
-    def __str__(self) -> str:
-        return self.value
-
-
-def build_chain(template: str) -> tuple[Node, Node]:
-    root: Node = None
-    previous: Node = None
-    for c in template:
-        current: Node = Node(c)
-        if root is None:
-            root = current
-        else:
-            previous.next = current
-            current.prev = previous
-        previous = current
-    return root, previous
-
-
 def parse_rules(lines: list[str]) -> dict[str, str]:
     rules: dict[str, str] = {}
     for line in lines:
@@ -61,55 +15,40 @@ def parse_rules(lines: list[str]) -> dict[str, str]:
     return rules
 
 
-def count_initial_nodes(root: Node, counts: dict[str, int]):
-    current: Node = root
-    while current is not None:
-        if current.value not in counts:
-            counts[current.value] = 0
-        counts[current.value] += 1
-        current = current.next
+def count_initial_chain(chain: str, counts: dict[str, int]):
+    for node in chain:
+        if node in counts:
+            counts[node] += 1
+        else:
+            counts[node] = 1
 
 
-def link(root: Node, rules: dict[str, str], counts: dict[str, int]):
-    current: Node = root
-    # no point in checking the last node, it can't match rules on it's own
-    while current.has_next():
-        if current.pair in rules:
-            link_node: Node = Node(rules[current.pair])
-            if link_node.value not in counts:
-                counts[link_node.value] = 0
-            counts[link_node.value] += 1
-            current.link(link_node)
-        current = current.next
-
-
-def chain(tail: Node):
-    current: Node = tail
-    while current is not None:
-        current.chain()
-        current = current.prev
-
-
-def print_polymer(step: int, root: Node):
-    polymer: str = ""
-    current: Node = root
-    while current is not None:
-        polymer += current.value
-        current = current.next
-    return f"{step}: {polymer}"
+def link(chain: str, rules: dict[str, str], counts: dict[str, int]) -> str:
+    linked_chain: str = ""
+    for i in range(len(chain) - 1):
+        pair: str = chain[i:i + 2]
+        if pair in rules:
+            node: str = rules[pair]
+            linked_chain = f"{linked_chain}{chain[i]}{node}"
+            if node in counts:
+                counts[node] += 1
+            else:
+                counts[node] = 1
+        else:
+            linked_chain = f"{linked_chain}{chain[i]}"
+    return f"{linked_chain}{chain[-1]}"
 
 
 def polymerise(input_file: str, steps: int) -> int:
     lines: list[str] = input.load_lines(DAY, input_file)
-    root, tail = build_chain(lines.pop(0))
+    chain: str = lines.pop(0)
     # get rid of the blank line
     lines.pop(0)
     rules: dict[str, str] = parse_rules(lines)
     counts: dict[str, int] = {}
-    count_initial_nodes(root, counts)
+    count_initial_chain(chain, counts)
     for _ in range(steps):
-        link(root, rules, counts)
-        chain(tail.prev)
+        chain = link(chain, rules, counts)
         print(_)
 
     maxn: int = max(counts.values())
